4ce6aadce0414e66f4bad93449b6e62b3a198462
Cody Walter <cody.walter@providencecc.edu>
2020-11-08 17:51:07 -0800

    created new array to hold data from csv file to access for chart

--- public/js/player.js
+++ public/js/player.js
@@ -2,6 +2,7 @@
 
 const selectedFile = document.getElementById("input").files[0];
 const inputElement = document.getElementById("input");
+const generateChart = document.getElementById("generate-chart");
 const numFiles = FileList.length;
 
 inputElement.addEventListener("change", handleFiles, false);
@@ -9,15 +10,57 @@ function handleFiles() {
   const fileList = this.files;
   console.log(fileList);
 }
-
+const metrics = [];
 let btn_upload = document
   .getElementById("upload-csv")
   .addEventListener("click", () => {
     Papa.parse(document.getElementById("input").files[0], {
       download: true,
       header: true,
-      complete: function (res, file) {
-        console.log(res, file);
+      complete: function (data) {
+        console.log(data);
+        metrics.push(data);
       },
     });
+    console.log(metrics);
   });
+
+generateChart.addEventListener("click", () => {
+  function generateChart() {
+    var chart = new CanvasJS.Chart("chartContainer", {
+      data: [
+        {
+          type: "scatter",
+          dataPoints: [
+            { x: 5, y: 10 },
+            { x: 20, y: 15 },
+            { x: 30, y: 25 },
+            { x: 40, y: 30 },
+            { x: 50, y: 28 },
+          ],
+        },
+      ],
+    });
+
+    chart.render();
+  }
+  generateChart();
+});
+//   window.onload = function () {
+//   var chart = new CanvasJS.Chart("chartContainer", {
+//     data: [
+//       {
+//         type: "scatter",
+//         dataPoints: [
+//           { x: 10, y: 10 },
+//           { x: 20, y: 15 },
+//           { x: 30, y: 25 },
+//           { x: 40, y: 30 },
+//           { x: 50, y: 28 },
+//         ],
+//       },
+//     ],
+//   });
+
+//   chart.render();
+// };
